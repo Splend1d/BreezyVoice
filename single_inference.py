862bbc64eb906c367960ef4379fb46e5da4be87f
許湛然(Jeff Hsu) <a24998667@gmail.com>
2025-01-23 18:03:33 +0800

    Update single_inference.py

--- single_inference.py
+++ single_inference.py
@@ -68,7 +68,7 @@ class CustomCosyVoiceFrontEnd(CosyVoiceFrontEnd):
         return texts
 
 ####model
-class CosyVoiceModelv2(CosyVoiceModel):
+class CustomCosyVoiceModel(CosyVoiceModel):
 
     def __init__(self,
                  llm: torch.nn.Module,
@@ -215,7 +215,7 @@ def main():
     
     parser.add_argument("--output_path", type=str, required=False, default="results/output.wav", help="Specifies the name and path for the output .wav file.")
     
-    parser.add_argument("--model_path", type=str, required=False, default = "Splend1dchan/BreezyVoice-300M",help="Specifies the model used for speech synthesis.")
+    parser.add_argument("--model_path", type=str, required=False, default = "MediaTek-Research/BreezyVoice-300M",help="Specifies the model used for speech synthesis.")
     args = parser.parse_args()
     
     
